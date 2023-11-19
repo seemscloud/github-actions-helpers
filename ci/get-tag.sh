b98dea833bb6fac57122c9922c90c918af7ac870
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2023-11-19 05:39:07 +0100

    sfad

--- ci/get-tag.sh
+++ ci/get-tag.sh
@@ -5,7 +5,7 @@ git fetch --tags
 VERSION_TAG=$(git tag --sort=committerdate  | grep -Ei "^[a-z0-9\-]{3,}-[0-9]+.[0-9]+.[0-9]+$" | grep -Ei "^${1}-" | tail -1)
 
 if [ -z "${VERSION_TAG}" ] ; then
-  exit 100
+  VERSION_TAG=notag
 fi
 
 echo ::set-output name=VERSION_TAG::"${VERSION_TAG}"
