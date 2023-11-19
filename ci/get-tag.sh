99c506fe0a04b92b43bbe7a3ed48cea4349b007c
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2023-11-19 03:58:31 +0100

    sfad

--- ci/get-tag.sh
+++ ci/get-tag.sh
@@ -2,13 +2,10 @@
 
 git fetch --tags
 
-LATEST_TAG=$(git tag --sort=committerdate  | grep -Ei "^[a-z0-9]{3,}-v[0-9]+.[0-9]+.[0-9]+$" | grep -Ei "^${1}-v" | tail -1)
+VERSION_TAG=$(git tag --sort=committerdate  | grep -Ei "^[a-z0-9]{3,}-v[0-9]+.[0-9]+.[0-9]+$" | grep -Ei "^${1}-v" | tail -1)
 
-if [ -z "${LATEST_TAG}" ] ; then
-  echo "Failed on tag match.."
+if [ -z "${VERSION_TAG}" ] ; then
   exit 100
 fi
 
-echo ::set-output name=LATEST_TAG::"${LATEST_TAG}"
-
-echo "${LATEST_TAG}"
+echo ::set-output name=VERSION_TAG::"${VERSION_TAG}"
