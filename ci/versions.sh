391fbab0b05199e4687f83bd98857c951052c72d
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2023-11-19 04:59:53 +0100

    sfad

--- ci/versions.sh
+++ ci/versions.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
 IMAGE_CHECK=$(echo "${GITHUB_REF}" | sed "s/refs\/tags\///g" | grep -Po "^[a-z0-9]+")
-
-echo "${IMAGE_CHECK}"
+IMAGE_VERSION=$(echo "debug-v1.0.2" | grep -Po "[a-z0-9]+-v\K.*")
 
 if [ "${IMAGE_CHECK}" == "${1}" ] ; then
   touch .versions
-  if [ ! `cat .versions | grep "${2}"` ] ; then
-    docker build -t "${DOCKER_REPO}/${1}:${2}" .
-    docker push "${DOCKER_REPO}/${1}:${2}"
+  if [ ! `cat .versions | grep "${IMAGE_VERSION}"` ] ; then
+    docker build -t "${DOCKER_REPO}/${1}:${IMAGE_VERSION}" .
+    docker push "${DOCKER_REPO}/${1}:${IMAGE_VERSION}"
 
     git checkout main
     git pull
 
     ls -lh
-    echo "${2}" >> .versions
+    echo "${IMAGE_VERSION}" >> .versions
     cat .versions | sort | uniq > .versions_BKP
     mv .versions_BKP .versions
     rm -f .versions_BKP
