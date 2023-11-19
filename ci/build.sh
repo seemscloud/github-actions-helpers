70774427853ce6b147edb2902ffca8387f791fd1
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2023-11-19 04:35:52 +0100

    sfad

--- ci/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-for i in `ls -1` ; do
-  echo "${i}"
-done
--- ci/versions.sh
+++ ci/versions.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
-echo docker build -t "${DOCKER_REPO}/${1}:${2}" .
+if [ -f .versions ] && [ $(cat debug/.versions | grep "${2}") ] ; then
+  echo docker build -t "${DOCKER_REPO}/${1}:${2}" .
+  echo docker push "${DOCKER_REPO}/${1}:${2}"
 
-echo docker push "${DOCKER_REPO}/${1}:${2}"
+  git checkout main
+  git pull
+
+  echo "${1}" >> .versions
+  cat .versions | sort | uniq > .versions_BKP
+  mv .versions_BKP .versions
+  rm -f .versions_BKP
+
+  git add .
+  git config --global user.name 'Versioner'
+  git config --global user.email 'contact@seems.cloud'
+  git commit -am "[Versioner] Bump .versions File" --allow-empty
+  git push
+fi
