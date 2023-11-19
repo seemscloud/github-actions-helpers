d887b7f4923296487385fa45a23bac994cc94c94
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2023-11-19 04:14:11 +0100

    sfad

--- ci/versions.sh
+++ ci/versions.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build -t "${DOCKER_REPO}/${1}:${2}" .
+echo docker build -t "${DOCKER_REPO}/${1}:${2}" .
 
-docker push "${DOCKER_REPO}/${1}:${2}"
+echo docker push "${DOCKER_REPO}/${1}:${2}"
