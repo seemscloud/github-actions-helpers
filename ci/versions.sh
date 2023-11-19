7e7369ac1c972b0a3268a2a78e43c9c1d05d3ddf
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2023-11-19 04:47:23 +0100

    sfad

--- ci/versions.sh
+++ ci/versions.sh
@@ -2,22 +2,27 @@
 
 IMAGE_CHECK=$(echo "${GITHUB_REF}" | sed "s/refs\/tags\///g" | grep -Po "^[a-z0-9]+")
 
-if [ "${IMAGE_CHECK}" == "${2}" ] && [ -f .versions ] && [ ! $(cat .versions | grep "${2}") ] ; then
-  echo docker build -t "${DOCKER_REPO}/${1}:${2}" .
-  echo docker push "${DOCKER_REPO}/${1}:${2}"
+echo "${IMAGE_CHECK}"
 
-  git checkout main
-  git pull
+if [ "${IMAGE_CHECK}" == "${2}" ] ; then
+  if [ "${IMAGE_CHECK}" == "${2}" ] && [ -f .versions ] && [ ! `cat .versions | grep "${2}"` ] ; then
+    echo docker build -t "${DOCKER_REPO}/${1}:${2}" .
+    echo docker push "${DOCKER_REPO}/${1}:${2}"
 
-  ls -lh
-  echo "${1}" >> .versions
-  cat .versions | sort | uniq > .versions_BKP
-  mv .versions_BKP .versions
-  rm -f .versions_BKP
+    git checkout main
+    git pull
 
-  git add .
-  git config --global user.name 'Versioner'
-  git config --global user.email 'contact@seems.cloud'
-  git commit -am "[Versioner] Bump .versions File" --allow-empty
-  git push
+    ls -lh
+    echo "${1}" >> .versions
+    cat .versions | sort | uniq > .versions_BKP
+    mv .versions_BKP .versions
+    rm -f .versions_BKP
+
+    git add .
+    git config --global user.name 'Versioner'
+    git config --global user.email 'contact@seems.cloud'
+    git commit -am "[Versioner] Bump .versions File" --allow-empty
+    git push
+  fi
 fi
+
